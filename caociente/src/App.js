eada53ac3c728eb542027e8b30bb9cc18679b357
ThierryAra <thierry_ara@usp.br>
2023-06-23 11:36:27 -0300

    create forms

--- caociente/src/App.js
+++ caociente/src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
-import Card from './components/Card';
+import Card from './components/Card/';
 import Footer from './components/Footer';
 import Header from './components/header/index.jsx';
+import Form from './components/Formulario';
 
 function App() {
   return (
     <div className="App">
-        <Card image={'/imagens/f0035.jpg'} name={'MAX'} />
-        <Footer />
       <Header />
+      {/* <Card image={'/imagens/f0035.jpg'} name={'MAX'} /> */}
+      <Form/>
+      <Footer />
     </div>
   );
 }
