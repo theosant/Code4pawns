55df1130589063cab35b7bb401406c86f10ba2d3
Théo <theomsantos@usp.br>
2023-06-23 11:50:06 -0300

    Adicionando Contatos

---
+++ caociente/src/components/Contato/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+
+const Contato = () => {
+  return (
+    <div className='contato'>
+        <a href="https://wa.me/+5516982355100">
+        <img className='contato-image' src="/imagens/whats.png" alt="Logo do Whats" />
+        </a>
+    </div>
+  )
+}
+
+export default Contato
