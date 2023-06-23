972edfddb99962d3b2c7261acda36943cd4d90f1
Théo <theomsantos@usp.br>
2023-06-23 11:23:14 -0300

    Footer e imagem do Canil

--- caociente/src/App.js
+++ caociente/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Card from './components/Card';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+        <Card image={'/imagens/f0035.jpg'} name={'MAX'} />
+        <Footer />
     </div>
   );
 }
---
+++ caociente/src/components/Footer/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        Feito por: <br />
+        Ketlen Victoria Martins de Souza <br />
+        Théo da Mota dos Santos <br />
+        Reynaldo Coronatto Neto <br />
+        Rebeca Viera Carvalho <br />
+        Thierry de Souza Araujo <br />
+    </div>
+  )
+}
+
+export default Footer
