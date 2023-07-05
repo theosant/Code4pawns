5c01c24a869a6beb4c11f565202eb05434175439
ThierryAra <thierry_ara@usp.br>
2023-07-05 12:03:44 -0300

    Feito Formulário de cadastro do cliente

--- caociente/src/App.js
+++ caociente/src/App.js
@@ -3,16 +3,20 @@ import Card from './components/Card';
 import Contato from './components/Contato';
 import Footer from './components/Footer';
 import Header from './components/header/index.jsx';
-import Form from './components/Formulario';
+import Form from './components/Formulario/QuizzCliente';
 import Home from './components/Home';
+import MeuFormulario from './components/Formulario/QuizzCliente';
+import CadastroCliente from './components/Formulario/CadastroCliente';
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <Home />
+        {/* <Home /> */}
         {/* <Card image={'/imagens/f0035.jpg'} name={'MAX'} /> */}
-        <Contato />
+        <Form/>
+        <CadastroCliente />
+        {/* <Contato /> */}
         <Footer />
     </div>
   );
